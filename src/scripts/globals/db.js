837f77e121e5c016e21ef88d65ca5918916d5216
massguunn <rohadigunawan0@gmail.com>
2024-06-15 22:07:48 +0800

    update DB total

--- src/scripts/globals/db.js
+++ src/scripts/globals/db.js
@@ -1,21 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('postgres', 'postgres.iqxymiztnxussxfhmfra', 'inicobaduluya', {
-  host: 'aws-0-ap-southeast-1.pooler.supabase.com', // Gunakan host dari pengaturan Supabase Anda
-  port: 5432, // Port default PostgreSQL
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Menghindari masalah dengan sertifikat SSL yang tidak dipercaya
-    }
-  },
-  logging: console.log, // Aktifkan logging untuk debugging
+const sequelize = new Sequelize("DB_PARIWISATA", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
 });
 
 sequelize
   .authenticate()
   .then(() => console.log("Database connected..."))
-  .catch((err) => console.error("Error: ", err.message, "\nDetails: ", err));
+  .catch((err) => console.log("Error: " + err));
 
 module.exports = sequelize;
